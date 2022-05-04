bde5a7ae024218c372f1b97a8cecb0c862e1a7ac
Alfredo Pereira <alfrededd@gmail.com>
2022-05-04 05:10:35 -0500

    cambio del puerto

--- app2.js
+++ app2.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port =3000;
+const port =process.env.PORT || 3000;
 
 // Motor de plantilla ejs en la carpeta views
 app.set("view engine", "ejs");
